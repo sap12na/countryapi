91a5cb525a1233485b518cb032e59735e06fa7ce
Sapna Shahi <80331079+sap12na@users.noreply.github.com>
2021-08-06 09:55:31 +0530

    Update api.js

--- api.js
+++ api.js
@@ -11,19 +11,6 @@ function newCountrySelection(event) {
   displayCountryInfo(event.target.value);
 }
 
-// fetch("https://restcountries.eu/rest/v2/all")
-// .then(function(res){
-//   // console.log(res);
-//   return res.json();
-// })
-// .then(function(data){
-//   // console.log(data);
-//   initialize(data);
-// })
-// .catch(function(err){
-//   console.log("Error:", err);
-// });
-
 fetch("https://restcountries.eu/rest/v2/all")
 .then(res => res.json())
 .then(data => initialize(data))
